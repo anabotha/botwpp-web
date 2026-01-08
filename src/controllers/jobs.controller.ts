26345379a11e4b943f7f8632df48a2cfa69a5613
anabotha <josefinabotha@gmail.com>
2026-01-08 14:55:52 -0300

    prueba de crons

--- src/controllers/jobs.controller.ts
+++ src/controllers/jobs.controller.ts
@@ -1,17 +1,15 @@
 import { ejecutarEvaluacionMercado } from "../services/market.service";
 import { Request, Response } from 'express';
 
+
 // export async function ejecutarEvaluacion(req: Request, res: Response) {
-//   const auth = req.headers.authorization;
+//   const secret = req.header("x-cron-secret");
 
-//   // 1. Verificación de Seguridad
-//   if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
-//     console.log("llego aca");
-
+//   if (secret !== process.env.CRON_SECRET) {
 //     return res.status(401).json({ error: "Unauthorized" });
 //   }
 
-//   try {
+// try {
 //     // await ejecutarEvaluacionMercado();
 //     console.log("llego aca al try");
 
@@ -28,28 +26,18 @@ import { Request, Response } from 'express';
 //     return res.status(500).json({ error: "Cron failed", detail: error.message });
 //   }
 // }
-
 export async function ejecutarEvaluacion(req: Request, res: Response) {
   const secret = req.header("x-cron-secret");
-
-  if (secret !== process.env.CRON_SECRET) {
+console.log("secret", secret);
+console.log("secret", process.env.CRON_SECRET);
+  if (!secret || secret !== process.env.CRON_SECRET) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
-try {
-    // await ejecutarEvaluacionMercado();
-    console.log("llego aca al try");
-
-    const resultado = await ejecutarEvaluacionMercado();
-
-    console.log("Cron ejecutado con éxito");
-    return res.json({
-      ok: true,
-      message: "Evaluación completada",
-      data: resultado // Opcional: para debug
-    });
-  } catch (error: any) {
-    console.error("Error en el proceso de Cron:", error.message, error);
-    return res.status(500).json({ error: "Cron failed", detail: error.message });
+  try {
+    console.log("Cron OK, entrando al controller");
+    return res.json({ ok: true });
+  } catch (error) {
+    return res.status(500).json({ error: "fail" });
   }
 }
